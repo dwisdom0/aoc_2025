b0c8ec962228c2121b6c5dbb182e8df9fc483f4c
dwisdom0 <2480711+dwisdom0@users.noreply.github.com>
2025-12-03 14:32:14 -0800

    add a comment to the day 3 part 2 solution

--- 03/solution2.py
+++ 03/solution2.py
@@ -26,6 +26,35 @@ def get_joltage(bank: list[int]) -> int:
     joltage_digits = ""
     start_idx = 0
 
+
+    # have to pick the max in a sliding window
+    # because we have to leave ourselves at least enough
+    # digits to turn on all 12
+    # so like if we have 15 digits
+    # we have to pick the largest in the first 3
+    # 
+    # |||
+    # ################
+    #  ^max   
+    # maybe it's here
+    # now we have to pick the largest from there to the first 4
+    # 
+    #   ||
+    # ################
+    #   ^max
+    # then from there to the first 5
+    #    ||
+    # ################
+    #     ^max
+    # then from there to the first 6
+    # but now we only have one choice from here
+    # until the end of the digits since we need 12 total
+    #      |
+    # ################
+    #      ^max
+    #
+    # could optimize by detecting this case and skipping to the end
+
     for cutoff in range(len(bank) - 12, len(bank)):
         joltage_digit, joltage_digit_idx = find_max_idx(bank[start_idx : cutoff + 1])
         joltage_digits += str(joltage_digit)
